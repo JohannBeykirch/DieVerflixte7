14d96ebf3e914e9795fc3c56ac4d02f2531bce0e
Johann Beykirch <beykirchjohann@gmail.com>
2022-05-12 14:05:25 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let punkte1
+let punkte2
+let summe1
+let summe2
+let wuerfel1
+let wuerfel2
+
+
+document.getElementById("würfel1").value = "Würfel1:" + wuerfel1
+document.getElementById("würfel2").value = "Würfel2:" + wuerfel2
+document.getElementById("punkte1").value = "Punkte Spieler1:" + punkte1
+document.getElementById("punkte2").value = "Punkte Spieler2:" + punkte2
+document.getElementById("summe1").value = summe1
+document.getElementById("summe2").value = summe2
